6cfe44436726531c4a9efe7df02de611c2704445
Sanja Daic <sanja.daic86@gmail.com>
2026-01-13 08:23:01 +0100

    React Router v6.4+

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import User from "./pages/User";
+import UserDetail from "./pages/UserDetail";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/user",
+    element: <User />,
+  },
+  {
+    path: "/user/:id",
+    element: <UserDetail />,
+  },
+]);
+
 const App = () => {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
