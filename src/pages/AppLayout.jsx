c97b8a28f9532608ae6ff9abd239728f8063ff89
Sanja Daic <sanja.daic86@gmail.com>
2026-01-13 09:54:49 +0100

    useNavigation

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,11 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Back from "../components/Back";
 import Menu from "../components/Menu";
+import Loader from "../components/Loader";
 
 const AppLayout = () => {
+  const navigation = useNavigation();
+
+  const isLoading = navigation.state === "loading";
+
   return (
     <div className="appLayoutWrapper">
       <Menu />
+      {isLoading && <Loader />}
       <main className="mainWrapper">
         <Outlet />
       </main>
